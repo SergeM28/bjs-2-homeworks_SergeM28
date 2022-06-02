2a092bd97ddc558d01581e10b11014550bbef856
Serge Mouradian <serge.muradian@gmail.com>
2022-06-02 20:17:57 +0300

    task.js added

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,24 @@
-function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+"use strict"
+  function solveEquation(a, b, c) {
+    let arr = {};
+    let d = b**2 - 4 * a * c;
+   console.log('D = ' + d);
+  
+   if (d < 0) {
+   arr = [];
 }
-
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
+    else if (d === 0) {
+      arr = (-b / (2 * a));
+    }
+    
+    else if (d > 0) { 
+        let res = [];
+        res.push((-b + Math.sqrt(d)) / (2 * a));
+        res.push((-b - Math.sqrt(d)) / (2 * a));
+        arr = res;
+    }
+    return arr;
 }
+console.log(solveEquation(3,24,36));
+
+
