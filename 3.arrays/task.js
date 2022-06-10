064f442a98514f523b213fd785f405358a0285e5
Serge Mouradian <serge.muradian@gmail.com>
2022-06-10 22:43:28 +0300

    homework for arrays added

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,18 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
-  return result; // boolean
+result = (arr1.length === arr2.length && arr1.every(function(value, index) { 
+  return value === arr2[index]}));
+  return result;
 }
+  console.log(compareArrays([2,2,3,4], [2,2,3,4]))
 
-function advancedFilter(arr) {
-  let resultArr;
 
-  // Ваш код
-
-  return resultArr; // array
-}
+  function advancedFilter(arr) {
+    return function(x) {
+      if (x > 0 && x % 3 === 0) {
+        return x * 10;
+      }
+    }
+  }
+   arr = [12, 5, 6, 13, 9];
+  console.log(arr.filter(advancedFilter()).map(advancedFilter()))
