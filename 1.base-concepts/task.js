9655827816c84de35e1905837c65e823147058e9
Serge Mouradian <serge.muradian@gmail.com>
2022-06-20 21:07:39 +0300

    fixed base-concepts added

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,58 +1,57 @@
 "use strict"
   function solveEquation(a, b, c) {
-    let arr = {};
+    let res = [];
     let d = b**2 - 4 * a * c;
    console.log('D = ' + d);
   
-   if (d < 0) {
-   arr = [];
-}
-    else if (d === 0) {
-      arr = (-b / (2 * a));
+    if (d === 0) {
+      res.push(-b / (2 * a));
     }
     
     else if (d > 0) { 
-        let res = [];
         res.push((-b + Math.sqrt(d)) / (2 * a));
         res.push((-b - Math.sqrt(d)) / (2 * a));
-        arr = res;
     }
-    return arr;
-}
-console.log(solveEquation(3,24,36));
-
-
-"use strict"
+   
+    else {
+      (d < 0);
+      res = [];
+    }
+    
+    return res;
+  }
+    console.log(solveEquation(3,24,36));
+   
+   
+    "use strict"
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-		
-  if (percent >= 0) {
-    percent = Number(percent) /100 /12;
+  let totalAmount = 0;
+  let loanRate = parseInt(percent) / 100;
+  let firstPay = parseInt(contribution);
+  let generalPrice = parseInt(amount);
+  
+  if (Number.isNaN(loanRate)) {
+    return totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
   }
-  else {
-    return (`"Параметр "Процентная ставка" содержит неправильное значение ${percent}”`)
+  else if (Number.isNaN(firstPay)) {
+    return totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  } 
+  else if (Number.isNaN(generalPrice)) {
+    return totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`; 
   }
-  if (contribution >= 0) {
-    contribution = Number(contribution);
+  let dateFrom = new Date();
+  let fullPay = 0;
+  if ((date.getFullYear() >= dateFrom.getFullYear()) && (date.getMonth() < 12) && (date.getMonth() >= 0) && (date.getDate() < 32) && (date.getDate() >= 1)) {
+  let creditAmount = amount - contribution;			
+let monthAmount = (date.getFullYear() - dateFrom.getFullYear()) * 12 + date.getMonth() - dateFrom.getMonth();
+      let payForYear = creditAmount * (loanRate / 12 * (1 + 1 / (Math.pow(1 + loanRate / 12, monthAmount) - 1)));			
+      fullPay = (payForYear * monthAmount).toFixed(2);
+      totalAmount = parseFloat(fullPay);
   }
-  else {
-    return (`"Параметр "Первоначальный взнос" содержит неправильное значение ${contribution}”`)
-  }
-		if (amount >= 0) {
-    amount = Number(amount);
-  }
-  else {
-    return (`"Параметр "Сумма кредита" содержит неправильное значение ${amount}”`)
-  }
-			let creditAmount = amount - contribution;			
-    let dateFrom = new Date();
-  			const nextYearDate = new Date(new Date().setFullYear(new Date().getFullYear() + 1)) 
-  let monthAmount = nextYearDate.getMonth() - dateFrom.getMonth() + (12 * (nextYearDate.getFullYear() - dateFrom.getFullYear()));
-  			let payForMonth = creditAmount * (percent + (percent / (((1 + percent)**monthAmount) - 1)));			
-  			 totalAmount = payForMonth * monthAmount;
-  			console.log(Math.floor(totalAmount * 100) / 100);
- 			return totalAmount;
-	
-}
-calculateTotalMortgage(10, 0, 50000, 12)
 
+  else {
+    totalAmount = 'Неверный формат даты. Дату необходимо указать в следующем формате - ДД.ММ.ГГГГ.';
+  }
+    
+  return totalAmount;
+}
